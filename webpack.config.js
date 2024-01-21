ae008409118ba85f38ec73b01a69256d8ed61d87
nlom0218 <nlom0218@naver.com>
2024-01-21 12:18:15 +0900

    study: 3d 물체에 그림자 적용

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 
 module.exports = {
   mode: 'production',
-  entry: './src/ex-06.ts',
+  entry: './src/ex-07.ts',
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
